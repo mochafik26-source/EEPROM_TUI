17286cbd02646940e1de537384b9c94e5205e2f6
mohamed chafik <mo.chafik26@gmail.com>
2026-08-29 18:20:49 +0100

    Working on the cli choices and input of values

--- main.go
+++ main.go
@@ -2,13 +2,82 @@ package main
 
 import(
     "fmt"
-    "time"
-    "go.bug.st/serial"
+		"strings"
+		"os"
 		tea "charm.land/bubbletea/v2"
+		"log"
+
+	"charm.land/bubbles/v2/textinput"
 	)
 
-func main(){
-	mode := &serial.Mode{
+	var choices = []string{"Add", "Read"}
+	type model struct {
+		cursor int
+		choice string
+	}
+
+	func (m model) Init() tea.Cmd{
+		return nil
+	}
+	func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+		switch msg := msg.(type) {
+		case tea.KeyPressMsg:
+			switch msg.String(){
+			case "up":
+				m.cursor++
+				if m.cursor >= len(choices){
+					m.cursor = 0
+				}
+			case "down":
+				m.cursor--
+				if m.cursor < 0{
+					m.cursor = len(choices) - 1
+				}
+			case "q":
+			return m, tea.Quit
+			case "enter":
+				return nil
+		}
+		
+		}
+		return m, nil
+	}
+func (m model) View() tea.View {
+	s := strings.Builder{}
+	s.WriteString("What kind of Bubble Tea would you like to order?\n\n")
+
+	for i := range choices {
+		if m.cursor == i {
+			s.WriteString("(•) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+	s.WriteString("\n(press q to quit)\n")
+
+	return tea.NewView(s.String())
+}
+	func main(){
+p := tea.NewProgram(model{})
+
+	// Run returns the model as a tea.Model.
+	m, err := p.Run()
+	if err != nil {
+		fmt.Println("Oh no:", err)
+		os.Exit(1)
+	}
+
+	// Assert the final tea.Model to our local model and print the choice.
+	if m, ok := m.(model); ok && m.choice != "" {
+		fmt.Printf("\n---\nYou chose %s!\n", m.choice)
+	}
+
+
+
+
+/*	mode := &serial.Mode{
 		BaudRate : 9600,
 	}
 
@@ -24,5 +93,7 @@ func main(){
  if err != nil{
 	 panic(err)
  }
- fmt.Println("done")
+ fmt.Println("done")*/
+
+ 
 }
