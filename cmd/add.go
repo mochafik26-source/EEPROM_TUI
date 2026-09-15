7ff4fc1b8ebf0f472916de6122e0d820c474d830
mohamed chafik <mo.chafik26@gmail.com>
2026-09-15 18:29:16 +0100

    finishing all functions

--- cmd/add.go
+++ cmd/add.go
@@ -1,41 +1,74 @@
 package cmd
-import(
+
+import (
 	"fmt"
+	"log"
+
 	"github.com/spf13/cobra"
 	"go.bug.st/serial"
 )
+
 var addCmd = &cobra.Command{
-	Use : "add",
-
+	Use:  "add",
 	Args: cobra.ExactArgs(3),
-	Run: func(cmd *cobra.Command, args []string){
-		Name := args[0]
-		Login := args[1]
-		Password := args[2]
-		fmt.Println(Name, Login, Password)
 
+	Run: func(cmd *cobra.Command, args []string) {
+
+		name := args[0]
+		login := args[1]
+		password := args[2]
+
+		// Open serial port
 		mode := &serial.Mode{
-		BaudRate: 9600,
-	}
+			BaudRate: 9600,
+		}
 
 		port, err := serial.Open("/dev/ttyUSB0", mode)
-	if err != nil {
-		panic(err)
-	}
-	defer port.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer port.Close()
 
-values := []string{Name, Login, Password}
+		// Data to send
+		message := "add|" + name + "|" + login + "|" + password + "\n"
 
-data := []byte("add" + "|" + values[0] + "|" + values[1] + "|" + values[2] + "|")
+		_, err = port.Write([]byte(message))
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		fmt.Println("Sent:", message)
 
-port.Write(data)
+		// -------------------------
+		// Read Arduino response
+		// -------------------------
 
-if err != nil {
-		panic(err)
-	}
+		buffer := make([]byte, 1)
+		var response []byte
 
-	fmt.Println("Sent:", values)
+		for {
 
+			n, err := port.Read(buffer)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if n > 0 {
+
+				// If Arduino sends '1',
+				// stop reading
+				if buffer[0] == '1' {
+					break
+				}
+
+				response = append(response, buffer[0])
+			}
+		}
+
+		fmt.Println("Arduino:", string(response))
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
 }
